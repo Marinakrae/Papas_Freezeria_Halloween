1a5156ac28acb59709d8faa479edef30e4b90f73
Marina Krause Eskelsen <marina.eskelsen@acad.ufsm.br>
2023-02-22 21:45:10 -0300

    Creditos e imagem

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Papas_Freezeria-Game
 Papa's Freezeria, mas é um jogo de verdade
+
+
+
+
+#Créditos
+Music by <a href="https://pixabay.com/users/geoffharvey-9096471/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=122118">geoffharvey</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=122118">Pixabay</a>
--- main.lua
+++ main.lua
@@ -1,26 +1,33 @@
 local screen = "mainMenu"
 
+love.window.setTitle("Papa's Freezeria Edição de Halloween")
+
+function love.conf(t)
+    t.screen.width = 900
+    t.screen.height = 700
+end
+
 function love.load()
-    fundo = love.graphics.newImage("assets/Fundo.png")
-    btnPlay = love.graphics.newImage("assets/btnPlay.png")
+    fundo = love.graphics.newImage("assets/fundo_menu_rascunho.png")
+    --btnPlay = love.graphics.newImage("assets/btnPlay.png")
 end
 
 function love.update(dt)
     if screen == "mainMenu" then
         -- Update main menu stuff.
-        if btnPlayIsPressed() then
-            screen = "orderStation"
-        end
+        -- if btnPlayIsPressed() then
+        --     screen = "orderStation"
+        -- end
     elseif screen == "orderStation" then
         -- Update in-game stuff.
-        if playerIsDead() then
-            screen = "mainmenu"
-        end
+        -- if playerIsDead() then
+        --     screen = "mainmenu"
+        -- end
     end
 end
 
 function love.draw()
-    if screen == "mainmenu" then
+    if screen == "mainMenu" then
         -- Define a cor de fundo da tela
         love.graphics.setBackgroundColor(255, 255, 255)
 
@@ -28,7 +35,7 @@ function love.draw()
         love.graphics.draw(fundo, 0, 0)
 
         -- Desenha o botão na posição desejada
-        love.graphics.draw(btnPlay, 100, 100)
+        --love.graphics.draw(btnPlay, 100, 100)
     elseif screen == "ingame" then
         -- Draw in-game stuff.
     end
